5b4e4518dfb2d99c38dd3259e1b8b91f425a105c
Z-beam <sasha.zbeam@gmail.com>
2024-08-03 19:48:36 +0300

    Update 11.ps1
    Змінив посилання для автоматичного завантаження оновленої Mova

--- 11.ps1
+++ 11.ps1
@@ -170,7 +170,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 	choco install zoom -y
 	
 # Встановлення Mova прямо у автозавантаження
-$exeUrl = "https://github.com/Z-beam/MovaFlag/releases/download/1.0.0/Mova.exe"
+$exeUrl = "https://github.com/Z-beam/MovaFlag/releases/download/1.0.1/Mova.exe"
 $downloadPath = "$env:USERPROFILE\Mova\Mova.exe"
 
 # Створення директорії для збереження файлу, якщо її ще не існує
